b004a51290979d4e8f58d9bf1f0ddac82dc43a35
Adam Winnicki <adam.baku@gmail.com>
2020-10-07 16:27:11 +0200

    add(common): add application exception

---
+++ src/Common/Exception/ApplicationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Exception
+{
+    public class ApplicationException : RuntimeExceptionAbstract
+    {
+        protected ApplicationException() : base()
+        {
+
+        }
+
+        protected ApplicationException(string? message) : base(message)
+        {
+
+        }
+
+        public ApplicationException(string? message, System.Exception? innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}

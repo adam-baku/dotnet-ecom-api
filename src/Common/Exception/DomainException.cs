b3399b4198fb7f1b0ab88cf6ede8ca865a0f8b92
Adam Winnicki <adam.baku@gmail.com>
2020-10-07 09:31:25 +0200

    feat(common): add runtime and domain exceptions

---
+++ src/Common/Exception/DomainException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Exception
+{
+    public class DomainException : RuntimeExceptionAbstract
+    {
+        protected DomainException() : base()
+        {
+
+        }
+
+        protected DomainException(string? message) : base(message)
+        {
+
+        }
+
+        public DomainException(string? message, System.Exception? innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ src/Common/Exception/RuntimeExceptionAbstract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Exception
+{
+    public abstract class RuntimeExceptionAbstract : SystemException
+    {
+        public RuntimeExceptionAbstract() : base()
+        {
+
+        }
+
+        public RuntimeExceptionAbstract(string? message) : base(message)
+        {
+
+        }
+
+        public RuntimeExceptionAbstract(string? message, System.Exception? innerException) : base(message, innerException)
+        {
+            
+        }
+    }
+}

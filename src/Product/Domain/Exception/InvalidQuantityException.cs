1cd4023aadd04c36322f1b06930af602fdf46e1c
Adam Winnicki <adam.baku@gmail.com>
2020-10-08 13:36:43 +0200

    feat(product): implement behavior of product entity

---
+++ src/Product/Domain/Exception/InvalidQuantityException.cs
@@ -0,0 +1,20 @@
+﻿using Common.Exception;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.Domain.Exception
+{
+    public class InvalidQuantityException : DomainException
+    {
+        private InvalidQuantityException(string message) : base(message)
+        {
+
+        }
+
+        public static InvalidQuantityException NonPositiveValue()
+        {
+            return new InvalidQuantityException("Quantity of product must be positive.");
+        }
+    }
+}

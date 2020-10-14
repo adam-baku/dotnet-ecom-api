889c16d2754f89c357a95db2a4aedf76ba97d2ef
Adam Winnicki <adam.baku@gmail.com>
2020-10-14 21:21:06 +0200

    feat(data): add abstract base repository class
    for repositories using a database

---
+++ src/Data/Repository/BaseDbRepositoryAbstract.cs
@@ -0,0 +1,15 @@
+﻿using baseDbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace Data.Repository
+{
+    public abstract class BaseDbRepositoryAbstract<TDbContext>
+        where TDbContext : baseDbContext
+    {
+        protected readonly TDbContext dbContext;
+
+        public BaseDbRepositoryAbstract(TDbContext db)
+        {
+            this.dbContext = db;
+        }
+    }
+}

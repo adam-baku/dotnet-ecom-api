6f66f822c4a7c87d872f03350703b45b98179da5
Adam Winnicki <adam.baku@gmail.com>
2020-10-14 12:22:50 +0200

    feat(data/product/common): add EF product db context with mapping
    Add paramless constructors to Product, Price, Tax classes
    Change type of Id property in Product entity

---
+++ src/Data/Mapping/ProductMap.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Mapping
+{
+    public class ProductMap : IEntityTypeConfiguration<Product.Domain.Product>
+    {
+        public void Configure(EntityTypeBuilder<Product.Domain.Product> builder)
+        {
+            builder.ToTable("Product");
+
+            builder.HasKey(p => p.Id);
+
+            builder.HasIndex(p => p.ProductId)
+                .IsUnique();
+
+            builder.HasIndex(p => p.Title)
+                .IsUnique();
+
+            builder.Property(p => p.ProductId)
+                .HasColumnName("ProductId")
+                .HasColumnType("varchar(50)")
+                .IsRequired();
+
+            builder.Property(p => p.Title)
+                .HasColumnName("Title")
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
+            builder.Property(p => p.AvailableQuantity)
+                .HasColumnName("AvailableQuantity")
+                .HasColumnType("int")
+                .IsRequired();
+
+            builder.OwnsOne(
+                p => p.Price,
+                price => {
+                    price.Property(p => p.Net)
+                        .HasColumnName("NetPrice")
+                        .HasColumnType("decimal(7,2)")
+                        .IsRequired();
+
+                    price.Property(p => p.Gross)
+                        .HasColumnName("GrossPrice")
+                        .HasColumnType("decimal(7,2)")
+                        .IsRequired();
+
+                    price.OwnsOne(
+                        p => p.Tax,
+                        tax => {
+                            tax.Property(p => p.Value)
+                                .HasColumnName("TaxValue")
+                                .HasColumnType("decimal(5,2)")
+                                .IsRequired();
+                        }
+                    );
+
+                    price.Property(p => p.Currency)
+                        .HasColumnName("Currency")
+                        .HasColumnType("varchar(3)")
+                        .IsRequired();
+                }
+            );
+        }
+    }
+}
--- src/Product/Domain/Product.cs
+++ src/Product/Domain/Product.cs
@@ -6,11 +6,14 @@ namespace Product.Domain
 {
     public class Product
     {
-        public int Id { get; private set; }
+        public long Id { get; private set; }
         public Guid ProductId { get; private set; }
         public string Title { get; private set; }
         public int AvailableQuantity { get; private set; }
         public Price Price { get; private set; }
+
+        //EF Core
+        private Product() { }
 
         public Product(string title, int availableQuantity, Price price)
         {

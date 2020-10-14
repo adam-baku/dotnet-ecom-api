1d4545cb4d25dfc3db26ed837197dbdd99e2cb40
Adam Winnicki <adam.baku@gmail.com>
2020-10-14 14:10:55 +0200

    feat(data): add create product table migrations

---
+++ src/Data/Migrations/20201013094251_CreateProductTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Title = table.Column<string>(type: "varchar(255)", nullable: false),
+                    AvailableQuantity = table.Column<int>(type: "int", nullable: false),
+                    NetPrice = table.Column<decimal>(type: "decimal(7,2)", nullable: true),
+                    GrossPrice = table.Column<decimal>(type: "decimal(7,2)", nullable: true),
+                    TaxValue = table.Column<decimal>(type: "decimal(5,2)", nullable: true),
+                    Currency = table.Column<string>(type: "varchar(3)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ProductId",
+                table: "Product",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Title",
+                table: "Product",
+                column: "Title",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
